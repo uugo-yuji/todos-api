89ac9e2be99cffc348fcf6d64b228dd9b6c83864
uugo-yuji <uugo.hosokai@gmail.com>
2019-11-14 10:29:23 +0900

    todo_model Test

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # TodoモデルがItemモデルと1:nの関係である
+  it { should have_many(:item).dependent(:destroy) }
+
+  # バリデーション：保存前にtitleカラムが存在する
+  it { should validate_presense_of(:title) }
+  # バリデーション:保存前にcreated_byカラムが存在する
+  it { should validate_presense_of(:created_by) }
 end
